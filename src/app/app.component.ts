a53ecc8652ed47d80e054a17dbe77ee2b624ca26
sarunas.ramanauskas <sarunas.ramanauskas@ibm.com>
2021-10-28 23:05:29 +0300

    Initial version

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, ViewChild } from '@angular/core';
+import { Chart, registerables } from 'chart.js';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,226 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'koch-fractal';
+
+  @ViewChild('canvas')
+  canvas: ElementRef<HTMLCanvasElement>;
+
+  @ViewChild('tooltip')
+  vertexTooltip: ElementRef<HTMLDivElement>;
+  tooltipText;
+
+  public context: CanvasRenderingContext2D;
+
+  constructor() {
+    Chart.register(...registerables);
+  }
+
+  message: string = "";
+
+  iteration: number = 2;
+
+  vertexes = [];
+
+  ngAfterViewInit() {
+    this.draw();
+    this.addCanvasEventListener();
+    this.reOffset();
+    window.onscroll = window.onresize = this.reOffset.bind(this);
+    this.hideVertexTooltip();
+  }
+
+  draw() {
+    this.vertexes = [];
+    const canvas = this.canvas.nativeElement;
+    const context = this.canvas.nativeElement.getContext('2d');
+    canvas.width = 1500
+    canvas.height = 480
+
+    const startingPoints = {
+        p1: {
+            x: 10,
+            y: 470
+        },
+        p2: {
+            x: 1488,
+            y: 470
+        }
+    }
+    
+
+    const koch = (a, b, limit = this.iteration, branch) => {
+
+        let [dx, dy] = [b.x - a.x, b.y - a.y]
+        let dist = Math.sqrt(dx * dx + dy * dy)
+        let unit = dist / 3
+        let angle = Math.atan2(dy, dx)
+
+        //This will be the triangular shape that makes the 'points' on the snowflake
+        let p1 = {
+            x: a.x + dx / 3,
+            y: a.y + dy / 3
+        }
+        let p3 = {
+            x: b.x - dx / 3,
+            y: b.y - dy / 3
+        }
+        let p2 = {
+            x: p1.x + Math.cos(angle - Math.PI / 3) * unit,
+            y: p1.y + Math.sin(angle - Math.PI / 3) * unit
+        }
+
+        if (limit > 0) {
+            // Decrease limit each time it's called
+            koch(a, p1, limit - 1, branch + '0');
+            koch(p1, p2, limit - 1, branch + '11');
+            koch(p2, p3, limit - 1, branch + '12');
+            koch(p3, b, limit - 1, branch + '2');
+        } else {
+            context.beginPath()
+            context.moveTo(a.x, a.y)
+            context.lineTo(p1.x, p1.y)
+            context.lineTo(p2.x, p2.y)
+            context.lineTo(p3.x, p3.y)
+            context.lineTo(b.x, b.y)
+            context.stroke()
+        }
+        this.registerVertexes([p1, p2, p3], branch);
+    }
+    koch(startingPoints.p1, startingPoints.p2, this.iteration, '');
+    this.registerVertex(startingPoints.p1.x, startingPoints.p1.y, 'start');
+    this.registerVertex(startingPoints.p2.x, startingPoints.p2.y, 'end');
+  }
+
+  registerVertexes(points, branch) {
+    for (let i in points) {
+      this.registerVertex(points[i].x, points[i].y, `${branch}${i}`);
+    }
+  }
+
+  registerVertex(x, y, branch) {
+    this.vertexes.push({
+      x, y, branch
+    });
+  }
+
+  drawVertexes() {
+    for (let v of this.vertexes) {
+      this.drawVertex(v);
+    }
+  }
+
+  drawVertex(vertex) {
+    let context = this.canvas.nativeElement.getContext('2d');
+    context.beginPath()
+    context.moveTo(vertex.x, vertex.y)
+    let radius = 7 - this.iteration;
+    context.arc(vertex.x, vertex.y, radius, 0, 2 * Math.PI);
+    context.fillStyle = "red";
+    context.fill();
+    context.moveTo(vertex.x, vertex.y);
+    context.closePath()
+  }
+
+  offsetX: number;
+  offsetY: number;
+
+  reOffset() {
+    var BB = this.canvas.nativeElement.getBoundingClientRect();
+    this.offsetX = BB.left;
+    this.offsetY = BB.top;
+  }
+
+  addCanvasEventListener() {
+    const canvas = this.canvas.nativeElement;
+    canvas.onmousemove = ($event) => {
+      let mouseX = $event.clientX - this.offsetX;
+      let mouseY = $event.clientY - this.offsetY;
+      
+      let hoverInterval = this.iteration >= 4 ? 2 : 10;
+      let targetedVertex = this.vertexes.find(v => Math.abs(v.x - mouseX) < hoverInterval && Math.abs(v.y - mouseY) < hoverInterval);
+
+      if (targetedVertex) {
+        this.draw();
+        this.drawVertex(targetedVertex);
+        this.displayVertexTooltip($event.clientX, $event.clientY, targetedVertex);
+        this.displayMessage(`Targeted vertex: ${targetedVertex.branch}`);
+      } else {
+        this.draw();
+        this.hideVertexTooltip();
+        this.clearMessage();
+      }
+    }
+  }
+
+  displayVertexTooltip(x, y, vertex) {
+    const TOOLTIP = this.vertexTooltip.nativeElement;
+    this.tooltipText = vertex.branch;
+    TOOLTIP.hidden = false;
+    TOOLTIP.style.left = `${Math.abs(x)}px`;
+    TOOLTIP.style.top = `${Math.abs(y) - 50}px`;
+  }
+
+  hideVertexTooltip() {
+    this.vertexTooltip.nativeElement.hidden = true;
+  }
+
+  next() {
+
+    if (this.iteration == 5) {
+      this.displayMessage("Can't get any higher due to resolution limitation!");
+      return;
+    }
+
+    this.clearMessage();
+
+    this.iteration = this.iteration + 1;
+    this.draw();
+  }
+
+  prev() {
+
+    if (this.iteration == 0) {
+      this.displayMessage("Can't get any lower!");
+      return;
+    }
+
+    this.clearMessage();
+
+    this.iteration = this.iteration - 1;
+    this.draw();
+  }
+
+  clearMessage() {
+    this.message = "";
+  }
+
+  displayMessage(message) {
+    this.message = message;
+  }
+
+  searchVertex:string = '';
+
+  clearSearch() {
+    this.searchVertex = '';
+  }
+
+  onKey(event: any) {
+
+    this.draw();
+
+    if (this.searchVertex !== '') {
+      let targetedVertex = this.vertexes.find(v => v.branch == this.searchVertex);
+
+      if (targetedVertex) {
+        this.drawVertex(targetedVertex);
+        this.displayMessage(`Targeted vertex: ${targetedVertex.branch}`);
+      } else {
+        this.displayMessage(`Cannot find ${this.searchVertex}`);
+      }
+    } else {
+      this.clearMessage();
+    }
+  }
 }
+
+
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,11 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms'; // <--- JavaScript import from 
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
@@ -10,7 +13,10 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    NgbModule,
+    FormsModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
