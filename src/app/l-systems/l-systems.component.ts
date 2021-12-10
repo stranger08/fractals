4f085fdbd1ee323f912aa5085e4b93c20c4df418
stranger08 <ramanauskassaras@gmail.com>
2021-12-10 12:10:56 +0200

    4th lab

--- src/app/l-systems/l-systems.component.ts
+++ src/app/l-systems/l-systems.component.ts
@@ -1,5 +1,8 @@
 import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
 import { ExamplesService } from '../examples.service';
+import { LSystemsService } from '../lsystems.service';
+
+import RealTurtle from 'real-turtle';
 
 @Component({
   selector: 'app-l-systems',
@@ -11,12 +14,21 @@ export class LSystemsComponent implements OnInit {
   @ViewChild('scene')
   scene: ElementRef<HTMLCanvasElement>;
   sceneNative;
-
   loading = false;
-  iterations: number = 0;
-  formula: string = "F"
+
+  settings = {
+    axiom: 'F',
+    iterations: 0,
+    formula: "F:F+F--F+F",
+    angle: 60,
+    length: "1300",
+    ratio: "0.333",
+    x: "100",
+    y: "500",
+  }
 
   constructor(
+    private lService: LSystemsService,
     private examplesService: ExamplesService,
   ) {
 
@@ -34,7 +46,7 @@ export class LSystemsComponent implements OnInit {
   async initScene() {
     this.sceneNative = this.scene.nativeElement;
     this.sceneNative.width = window.innerWidth * 94/100;
-    this.sceneNative.height = window.innerHeight * 8/10;
+    this.sceneNative.height = window.innerHeight * 7/10;
     this.redraw();
   }
 
@@ -46,13 +58,41 @@ export class LSystemsComponent implements OnInit {
   }
 
   draw() {
-      let context = this.sceneNative.getContext('2d');
-      context.beginPath();
-      context.moveTo(0, 0);
-      context.lineTo(100, 100);
-      context.strokeStyle = "white";
-      context.stroke();
-  }
+      let paintInstructions = this.lService.produce(this.settings.axiom, this.settings.formula, this.settings.iterations);
+      console.log(paintInstructions);
+      let turtle = new RealTurtle(this.sceneNative, {});
+      turtle.setIcon("");
+      turtle.setSpeed(1);
+      turtle.setPosition(eval(this.settings.x), eval(this.settings.y));
+      turtle.penDown();
+      turtle.beginPath();
+      turtle.right(90);
+      let stepSize = this.settings.iterations > 0
+        ? eval(this.settings.length) * eval(this.settings.ratio)**this.settings.iterations
+        : eval(this.settings.length);
+      for (let c of paintInstructions) {
+        switch (c) {
+          case 'F':
+            turtle.forward(stepSize);
+            break;
+          case 'I':
+            turtle.penUp();
+            turtle.forward(stepSize);
+            turtle.penDown();
+            break;
+          case '+':
+            turtle.left(this.settings.angle);
+            break;
+          case '-':
+            turtle.right(this.settings.angle);
+            break;
+          default:
+            continue;
+        }
+      }
+      turtle.start();
+      turtle.closePath();
+    }
  
 
   scenePosition(point) {
@@ -68,4 +108,28 @@ export class LSystemsComponent implements OnInit {
     let context = this.sceneNative.getContext('2d');
     context.clearRect(0, 0, this.sceneNative.width, this.sceneNative.height);
   }
+
+  examples:any = [
+    {
+      name: "Koch",
+      settings: this.lService.koch(),
+    },
+    {
+      name: "Quadratic",
+      settings: this.lService.quadratic(),
+    },
+    {
+      name: "Snowflake",
+      settings: this.lService.snowflake(),
+    },
+    {
+      name: "Cantor",
+      settings: this.lService.cantor(),
+    },
+  ];
+
+  loadExamples(i) {
+    this.settings = this.examples[i].settings;
+  }
+
 }
--- src/app/lsystems.service.ts
+++ src/app/lsystems.service.ts
@@ -6,4 +6,90 @@ import { Injectable } from '@angular/core';
 export class LSystemsService {
 
   constructor() { }
+
+  rules(reproduction) {
+    const rule_set = reproduction.split(',');
+
+    return rule_set.map(raw_rule => {
+      const rule_details = raw_rule.split(':');
+      return {
+        in: rule_details[0],
+        out: rule_details[1],
+      }
+    });
+  }
+
+  produce(axiom, reproduction, iteration) {
+
+    if (iteration <= 0) {
+      return axiom;
+    }
+
+    let retVal = '';
+    let rules = this.rules(reproduction);
+    console.log(rules);
+
+    for (let c of axiom) {
+      const rule = rules.find(r => r.in == c);
+      if (rule) {
+        retVal = retVal + rule.out;
+      } else {
+        retVal = retVal + c;
+      }
+    }
+
+    return  this.produce(retVal, reproduction, iteration - 1);;
+  }
+
+  koch() {
+    return {
+        axiom: 'F',
+        iterations: 0,
+        formula: "F:F+F--F+F",
+        angle: 60,
+        length: "1300",
+        ratio: "0.333",
+        x: "100",
+        y: "500",
+    }
+  }
+
+  snowflake() {
+    return {
+        axiom: '+F--F--F',
+        iterations: 0,
+        formula: "F:F+F--F+F",
+        angle: 60,
+        length: "200",
+        ratio: "0.4",
+        x: "700",
+        y: "400",
+    }
+  }
+
+  quadratic() {
+    return {
+        axiom: 'F+F+F+F',
+        iterations: 0,
+        formula: "F:F+F-F-FF+F+F-F",
+        angle: 90,
+        length: "200",
+        ratio: "0.3",
+        x: "700",
+        y: "500",
+    }
+  }
+  
+  cantor() {
+    return {
+      axiom: 'F',
+      iterations: 0,
+      formula: "F:FIF,I:III",
+      angle: 0,
+      length: "1300",
+      ratio: "1/3",
+      x: "200",
+      y: "500",
+    }
+  }
 }
