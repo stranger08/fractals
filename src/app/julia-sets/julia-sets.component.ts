81f2f222df615a718ad8c00ec3f569c4a3b93262
stranger08 <ramanauskassaras@gmail.com>
2021-12-28 18:06:15 +0200

    7th lab Mandelbrot sets

--- src/app/julia-sets/julia-sets.component.ts
+++ src/app/julia-sets/julia-sets.component.ts
@@ -11,7 +11,6 @@ export class JuliaSetsComponent implements OnInit {
 
   constructor() { }
 
-
   ngOnInit(): void {
   }
 
@@ -19,15 +18,20 @@ export class JuliaSetsComponent implements OnInit {
     this.initScene();
   }
 
-  @ViewChild('scene')
-  scene: ElementRef<HTMLCanvasElement>;
-  sceneNative;
-  chart: Chart;
+  @ViewChild('mandelbrot')
+  mandelbrot: ElementRef<HTMLCanvasElement>;
+
+  @ViewChild('julia')
+  julia: ElementRef<HTMLCanvasElement>;
 
   async initScene() {
-    this.sceneNative = this.scene.nativeElement;
-    let context = this.sceneNative.getContext('2d');
-    context.canvas.width = window.innerWidth * 40/100;
+    this.setDefaultDimensions(this.getContext(this.mandelbrot));
+    this.setDefaultDimensions(this.getContext(this.julia));
+    this.addCanvasEventListener();
+  }
+
+  setDefaultDimensions(context) {
+    context.canvas.width = window.innerWidth * 20/100;
     context.canvas.height = context.canvas.width;
   }
 
@@ -37,41 +41,68 @@ export class JuliaSetsComponent implements OnInit {
     this.loading = !!show;
   }
 
+  getContext(ref) {
+    return ref.nativeElement.getContext('2d');
+  }
+
   clear() {
-    let context = this.sceneNative.getContext('2d');
-    context.clearRect(0, 0, this.sceneNative.width, this.sceneNative.height);
+    let context = this.getContext(this.mandelbrot);
+    context.clearRect(0, 0, context.canvas.width, context.canvas.height);
+    context =  this.getContext(this.julia);
+    context.clearRect(0, 0, context.canvas.width, context.canvas.height);
   }
 
-  redraw() {
-    this.showSpinner(true);
-    this.clear();
-    setTimeout(() => {
-      this.draw();
-      this.showSpinner(false);
-    }, 1);
-  }
-
-  size:number=2;
+  size:number=1;
   gridX0:number = -1;
   gridX1:number = 1;
   gridY0:number = -1;
   gridY1:number = 1;
 
-  draw() {
-    let context = this.sceneNative.getContext('2d');
+  redrawMandelbrot() {
+    this.showSpinner(true);
+    setTimeout(() => {
+      this.drawMandelbrot();
+      this.showSpinner(false);
+    }, 1);
+  }
 
-    for (let x = 0; x < context.canvas.width; x = x + 1) {
-      for (let y = 0; y < context.canvas.height; y = y + 1) {
+  redrawJulia() {
+    this.showSpinner(true);
+    setTimeout(() => {
+      this.drawJulia();
+      this.showSpinner(false);
+    }, 1);
+  }
+
+  drawMandelbrot() {
+    let context = this.getContext(this.mandelbrot);
+
+    for (let x = 0; x < context.canvas.width; x = x + this.size) {
+      for (let y = 0; y < context.canvas.height; y = y + this.size) {
           let z = this.toComplex(x, y);
-          context.fillStyle = this.escape(z);
-          context.fillRect(x, y, 1, 1);
+          // Maldelbrot set
+          context.fillStyle = this.escape(MathJS.complex(0, 0), z);
+          context.fillRect(x, y, this.size, this.size);
+      }
+    }
+  }
+
+  drawJulia() {
+    let context = this.getContext(this.julia);
+
+    for (let x = 0; x < context.canvas.width; x = x + this.size) {
+      for (let y = 0; y < context.canvas.height; y = y + this.size) {
+          let z = this.toComplex(x, y);
+          // Julia set
+          context.fillStyle = this.escape(z, MathJS.complex(this.lambda));
+          context.fillRect(x, y, this.size, this.size);
       }
     }
   }
 
   escapeRadius:number = 2;
 
-  escape(z) {
+  escape(z, l) {
     const ESCAPE_LEVEL = [
       '#c1c5f5',
       '#abafeb',
@@ -119,7 +150,7 @@ export class JuliaSetsComponent implements OnInit {
     ].reverse();
 
     for (let i = 0; i < ESCAPE_LEVEL.length; i++) {
-      z = this.f(z);
+      z = this.f(z, l);
       if (!(z.re**2 + z.im**2 < this.escapeRadius**2)) {
         return ESCAPE_LEVEL[i];
       }
@@ -129,7 +160,7 @@ export class JuliaSetsComponent implements OnInit {
   }
 
   toComplex(x, y) {
-    let context = this.sceneNative.getContext('2d');
+    let context = this.getContext(this.mandelbrot);
     let REAL_SET = { start: this.gridX0, end: this.gridX1 }
     let IMAGINARY_SET = { start: this.gridY0, end: this.gridY1 }
     const WIDTH = context.canvas.width;
@@ -143,10 +174,36 @@ export class JuliaSetsComponent implements OnInit {
     return MathJS.complex(z.x, z.y);
   }
 
-  lambda:string = '-0.54 + 0.54i';
+  lambda:string = '-0.54 + 0.525911i';
+  fz:string = "1";
 
-  f(z):any {
-    return MathJS.add(MathJS.add(MathJS.pow(z, 7), MathJS.pow(z, 3)), MathJS.complex(this.lambda));// -0.70176 - 0.3842i
-    // return MathJS.add(MathJS.pow(z, 2), MathJS.complex(this.lambda));// -0.835 - 0.2321i for cool expiriences :) 
+  f(z, l):any {
+    switch (parseInt(this.fz)) {
+      case 1:
+        return MathJS.add(MathJS.add(MathJS.pow(z, 7), MathJS.pow(z, 3)), l);
+      case 2:
+        return MathJS.add(MathJS.pow(z, 2), l);
+      case 3:
+        return MathJS.add(MathJS.subtract(MathJS.pow(z, 4), MathJS.pow(z, 2)), l);
+      default:
+        return MathJS.add(MathJS.add(MathJS.pow(z, 7), MathJS.pow(z, 3)), l);
+    }
   }
+
+  addCanvasEventListener() {
+    this.getContext(this.mandelbrot).canvas.onclick = ($event) => {
+      console.log($event);
+
+      let bounds = $event.target.getBoundingClientRect();
+      let offsetX = bounds.left;
+      let offsetY = bounds.top;
+
+      let mouseX = $event.clientX - offsetX;
+      let mouseY = $event.clientY - offsetY;
+
+      this.lambda = this.toComplex(mouseX, mouseY).toString();
+    }
+  }
+
+
 }
